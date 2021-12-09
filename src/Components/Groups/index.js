4cde65ea33c9bfee59757f8f9964c4c14ed32586
Samuel Manga Rodrigues <samuelmanga22222222@outlook.com>
2021-12-09 10:04:47 -0300

    feat:CriaçãoDeGrupos

--- src/Components/Groups/index.js
+++ src/Components/Groups/index.js
@@ -1,12 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { GroupContext } from "../../Providers/Groups";
+import { useForm } from "react-hook-form";
 
 const Groups = () => {
-  const { request, groups } = useContext(GroupContext);
-  console.log(groups);
+  const { register, handleSubmit } = useForm({});
+  const { request, requestSub, groups, subscribe, setInput } =
+    useContext(GroupContext);
+  const [inpt, setInpt] = useState("");
+  useEffect(() => {});
+  const subValue = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <button onClick={request}>clique</button>
+      <div>crie um grupo</div>
+      <form onSubmit={handleSubmit(subValue)}>
+        <input {...register("name")} placeholder="Nome do grupo" />
+        <input {...register("description")} placeholder="Descrição do grupo" />
+        <input {...register("category")} placeholder="Categoria do grupo" />
+        <button type="submit">Criar grupo</button>
+      </form>
+      <div>
+        <ul>
+          {groups.map((group) => (
+            <>
+              <li>
+                {group.name}
+                <button onClick={() => requestSub(group.id)}>
+                  {" "}
+                  Increver-se
+                </button>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
+      <input onChange={(e) => setInpt(e.target.value)} />
+      <button onClick={() => setInput(inpt)}>clique</button>
     </div>
   );
 };
--- src/Providers/Groups/index.js
+++ src/Providers/Groups/index.js
@@ -4,21 +4,69 @@ export const GroupContext = createContext();
 
 export const GroupsProvider = ({ children }) => {
   const [groups, setGroups] = useState([]);
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [responses, setResponse] = useState([]);
+  const [subs, setSubs] = useState([]);
   const token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM5MTY2MzcyLCJqdGkiOiI4ZWE2ZjJiMWQwZGU0NDJmYTQ1NTBhMDZjZGJlMGRmMCIsInVzZXJfaWQiOjE5fQ.UCTZiSdcxlyNhjqlGhCDann5MmF1taQqqSajKGc-i8A";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM5NDg2MTMxLCJqdGkiOiI5ZWI0MjRhMjNhYWI0NTUwYmVhY2Y4ZGUxZjUyZGQ4ZiIsInVzZXJfaWQiOjk1fQ.ghNkwRw4jpobDI0FrB-CSDNG3R3_eYR2IC8CxcdhrLo";
   const request = () => {
     api
-      .get("/groups", {
-        headers: { Authorization: `Bearer ${token}` },
+      .get(`/groups/?search=${input}`, {
+        headers: { Authorization: `Bearer: ${token}` },
       })
       .then((response) => {
-        setGroups(response.data);
+        setGroups(response.data.results);
       });
+  };
+  const requestSub = (groupId) => {
+    api
+      .post(`/groups/${groupId}/subscribe/`, {
+        headers: { Authorization: `Bearer: ${token}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const subscribe = () => {
+    api
+      .get("/groups/subscriptions/", {
+        headers: { Authorization: `Bearer: ${token}` },
+      })
+      .then((response) => console.log(response.data));
+  };
+  const addRequest = () => {
+    api
+      .post(
+        "/groups",
+        {
+          name: "Grupinho Testando123",
+          description: "aisodhjaioj ",
+          category: "categoryaskdmal",
+        },
+        {
+          headers: { Authorization: `Bearer: ${token}` },
+        }
+      )
+      .then((response) => console.log(response));
   };
   useEffect(() => {
     request();
-  }, []);
+  });
   return (
-    <GroupContext.Provider value={request}>{children}</GroupContext.Provider>
+    <GroupContext.Provider
+      value={{
+        groups,
+        request,
+        setInput,
+        requestSub,
+        subscribe,
+      }}
+    >
+      {children}
+    </GroupContext.Provider>
   );
 };
