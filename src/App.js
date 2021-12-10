b8c17a1dbf5eb55e6155224f2a623e7af104c9c8
Matheus Serafim <matheus18serafim@gmail.com>
2021-12-10 17:45:54 -0300

    fix:Updating the branch featPagesRegister

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import { Routes } from "./Routes/index";
-import { GlobalStyles } from "./Styles/GlobalStyles";
 
-<<<<<<< HEAD
-const App = () => {
-  return (
-    <>
-      <Routes />
-    </>
-  );
-};
-=======
 const App = () => (
   <>
-    <GlobalStyles />
     <Routes />
   </>
 );
->>>>>>> featPagesRegisterLeandro
 
 export default App;
