5a47b84d5c2e5437ea799892f88c51a64406b061
Matheus Serafim <matheus18serafim@gmail.com>
2021-12-10 17:38:48 -0300

    Feat: Updating the branch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-=======
 import { Routes } from "./Routes/index";
->>>>>>> origin
-
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      
-    </div>
-=======
     <>
       <Routes />
     </>
->>>>>>> origin
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyles } from "./Styles/GlobalStyles";
 import { Providers } from "./Context/index";
->>>>>>> origin
 
 ReactDOM.render(
   <React.StrictMode>
