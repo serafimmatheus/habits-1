151c269a3108f7e07b202e8f699f121b2a9abb59
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2021-12-16 21:11:24 -0400

    fix: conflict home page

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -6,29 +6,6 @@ import { useHistory } from "react-router-dom";
 export const Home = () => {
   const history = useHistory();
   return (
-<<<<<<< HEAD
-    <MainHome>
-      <Container>
-        <div className="main-home__title">
-          <h1>Good</h1>
-          <h1>Habits</h1>
-        </div>
-
-        <div className="main-home__buttons">
-          <Button onClick={() => history.push("/register")} whiteSchema>
-            Cadastre-se
-          </Button>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </div>
-
-        <span className="main-home__span">
-          “Se você acredita que pode mudar – se faz disso um hábito -, a mudança
-          se torna real. Este é o verdadeiro poder do hábito.”
-          <em> Charles Duhigg</em>
-        </span>
-      </Container>
-    </MainHome>
-=======
     <>
       <MainHome>
         <Container>
@@ -123,6 +100,5 @@ export const Home = () => {
         </ul>
       </FooterHome>
     </>
->>>>>>> origin/featFooter
   );
 };
